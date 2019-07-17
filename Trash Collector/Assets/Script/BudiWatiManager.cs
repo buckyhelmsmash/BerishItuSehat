f5dbe264042ed40a20012ad7d69a7de7e1392b8a
buckyhelmsmash <49297253+buckyhelmsmash@users.noreply.github.com>
2019-07-17 10:07:12 +0700

    Revisi

--- Trash Collector/Assets/Script/BudiWatiManager.cs
+++ Trash Collector/Assets/Script/BudiWatiManager.cs
@@ -24,4 +24,5 @@ public class BudiWatiManager : MonoBehaviour
     {
         Application.Quit();
     }
+
 }
---
+++ Trash Collector/Assets/kucing.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kucing : MonoBehaviour
+{
+    public float Speed;
+    public Vector3 start, finish;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < start.x)
+        {
+            Right();
+        }
+        if (transform.position.x > finish.x)
+        {
+            Left();
+        }
+
+    }
+    void Right()
+    {
+        transform.Translate(Vector2.right * Speed * Time.deltaTime);
+    }
+    void Left()
+    {
+        transform.Translate(Vector2.left * Speed * Time.deltaTime);
+    }
+}
