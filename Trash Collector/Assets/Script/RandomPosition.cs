d63c27c7b032d223b67c62c4116cb2e48e09087b
alfanjuni <alfanjuni@gmail.com>
2019-07-20 14:05:53 +0700

    random sampah

---
+++ Trash Collector/Assets/Script/RandomPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3[] positions;
+    void Start()
+    {
+        int randomNumber = Random.Range(0,positions.Length);
+        transform.position = positions[randomNumber];
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
